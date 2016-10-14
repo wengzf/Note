ccbc520befcb2668b02b5a6ead2791a00c530d8f
wengzhifang <wengzhifang@yonglibao.com>
2016-10-14 18:37:24 +0800

    no message

--- SystemNavigation/SystemNavigation/VSCOAnimation.m
+++ SystemNavigation/SystemNavigation/VSCOAnimation.m
@@ -16,7 +16,7 @@
     {
         CGMutablePathRef path = CGPathCreateMutable();
         CGPathAddRect(path, nil, CGRectMake(44, 128, 232, 232));
-        
+    
         CAShapeLayer *layer = [CAShapeLayer layer];
         layer.path = path;
         layer.fillColor = [UIColor clearColor].CGColor;
@@ -27,18 +27,47 @@
         CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"lineWidth"];
         animation.fromValue = @2;
         animation.toValue = @10;
-        animation.duration = 2;
+        animation.duration = 1;
         animation.fillMode = kCAFillModeBoth;
         
         animation.removedOnCompletion = NO;
         [layer addAnimation:animation forKey:nil];
         
-//        CABasicAnimation *animation2 = [CABasicAnimation animationWithKeyPath:@"strokeColor"];
-//        animation2.fromValue = [UIColor lightGrayColor];
-//        animation2.toValue = [UIColor blackColor];
-//        animation2.duration = 2;
-//        [layer addAnimation:animation2 forKey:nil];
-//        
+        CABasicAnimation *animation2 = [CABasicAnimation animationWithKeyPath:@"strokeColor"];
+        animation2.fromValue = [UIColor lightGrayColor];
+        animation2.toValue = [UIColor blackColor];
+        animation2.duration = 1;
+        [layer addAnimation:animation2 forKey:nil];
+    }
+    
+    // 圆形出现
+    {
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathAddEllipseInRect(path, nil, CGRectMake(112, 196, 94, 94));
+        
+        CAShapeLayer *layer = [CAShapeLayer layer];
+        layer.path = path;
+        layer.fillColor = [UIColor clearColor].CGColor;
+        layer.strokeColor = [UIColor lightGrayColor].CGColor;
+        layer.fillMode = kCAFillModeBoth;
+        [self.layer addSublayer:layer];
+        
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"lineWidth"];
+        animation.fromValue = @1;
+        animation.toValue = @3;
+        animation.duration = 1;
+        animation.beginTime = CACurrentMediaTime()+3.0;
+        animation.fillMode = kCAFillModeBoth;
+        
+        animation.removedOnCompletion = NO;
+        [layer addAnimation:animation forKey:nil];
+        
+        CABasicAnimation *animation2 = [CABasicAnimation animationWithKeyPath:@"strokeColor"];
+        animation2.fromValue = [UIColor lightGrayColor];
+        animation2.toValue = [UIColor blackColor];
+        animation2.duration = 1;
+        animation2.beginTime = CACurrentMediaTime()+4.0;
+        [layer addAnimation:animation2 forKey:nil];
     }
     
     
